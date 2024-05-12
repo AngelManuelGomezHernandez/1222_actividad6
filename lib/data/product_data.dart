781b5d1ccce843d7342021ba89080ea8379275a6
ANGEL_MANUEL_GOMEZ_HERNANDEZ <a.21308051281222@cbtis128.edu.mx>
2024-05-12 23:32:15 +0000

    actualizacion

---
+++ lib/data/product_data.dart
@@ -0,0 +1,37 @@
+import 'package:gomez1222_act6/modelos/product_model.dart';
+
+List<ProductModel> products = [
+  ProductModel(
+    img: "assets/Aceite.png",
+    id_producto: "ID: 101",
+    nombre_producto: "Nombre: Aceite",
+    id_departamento: "ID Departamento: 1",
+    proveedor: "123 productos",
+    precio: "Precio: 43",
+    precio_total: "Precio Total: 53",
+    iva: "IVA: 10",
+    id_proveedor: "ID Proveedor: 10",
+  ),
+  ProductModel(
+    img: "assets/Arroz.png",
+    id_producto: "ID: 102",
+    nombre_producto: "Nombre: Arroz",
+    id_departamento: "ID Departamento: 2",
+    proveedor: "Tlaloc productos",
+    precio: "Precio: 23",
+    precio_total: "Precio Total: 33",
+    iva: "IVA: 10",
+    id_proveedor: "ID Proveedor: 11",
+  ),
+  ProductModel(
+    img: "assets/AtunDolores.png",
+    id_producto: "ID: 103",
+    nombre_producto: "Nombre: Atun",
+    id_departamento: "ID Departamento: 3",
+    proveedor: "Dolores",
+    precio: "Precio: 12",
+    precio_total: "Precio Total: 22",
+    iva: "IVA: 10",
+    id_proveedor: "ID Proveedor: 12",
+  ),
+];
